24e2f82db599925b891261736f363b4acefaa5ee
Jeremy Hatton <jhatton204@gmail.com>
2024-01-30 22:23:53 -0600

    Consuming API to provide Hiragana characters instead of manually defining them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,108 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-	const hiragana = [
-		{ romanji: 'a', hiragana: 'あ' },
-		{ romanji: 'i', hiragana: 'い' },
-		{ romanji: 'u', hiragana: 'う' },
-		{ romanji: 'e', hiragana: 'え' },
-		{ romanji: 'o', hiragana: 'お' },
-		{ romanji: 'ka', hiragana: 'か' },
-		{ romanji: 'ki', hiragana: 'き' },
-		{ romanji: 'ku', hiragana: 'く' },
-		{ romanji: 'ke', hiragana: 'け' },
-		{ romanji: 'ko', hiragana: 'こ' },
-		{ romanji: 'sa', hiragana: 'さ' },
-		{ romanji: 'shi', hiragana: 'し' },
-		{ romanji: 'su', hiragana: 'す' },
-		{ romanji: 'se', hiragana: 'せ' },
-		{ romanji: 'so', hiragana: 'そ' },
-		{ romanji: 'ta', hiragana: 'た' },
-		{ romanji: 'chi', hiragana: 'ち' },
-		{ romanji: 'tsu', hiragana: 'つ' },
-		{ romanji: 'te', hiragana: 'て' },
-		{ romanji: 'to', hiragana: 'と' },
-		{ romanji: 'na', hiragana: 'な' },
-		{ romanji: 'ni', hiragana: 'に' },
-		{ romanji: 'nu', hiragana: 'ぬ' },
-		{ romanji: 'ne', hiragana: 'ね' },
-		{ romanji: 'no', hiragana: 'の' },
-		{ romanji: 'ha', hiragana: 'は' },
-		{ romanji: 'hi', hiragana: 'ひ' },
-		{ romanji: 'fu', hiragana: 'ふ' },
-		{ romanji: 'he', hiragana: 'へ' },
-		{ romanji: 'ho', hiragana: 'ほ' },
-		{ romanji: 'ma', hiragana: 'ま' },
-		{ romanji: 'mi', hiragana: 'み' },
-		{ romanji: 'mu', hiragana: 'む' },
-		{ romanji: 'me', hiragana: 'め' },
-		{ romanji: 'mo', hiragana: 'も' },
-		{ romanji: 'ya', hiragana: 'や' },
-		{ romanji: 'yu', hiragana: 'ゆ' },
-		{ romanji: 'yo', hiragana: 'よ' },
-		{ romanji: 'ra', hiragana: 'ら' },
-		{ romanji: 'ri', hiragana: 'り' },
-		{ romanji: 'ru', hiragana: 'る' },
-		{ romanji: 're', hiragana: 'れ' },
-		{ romanji: 'ro', hiragana: 'ろ' },
-		{ romanji: 'wa', hiragana: 'わ' },
-		{ romanji: 'wo', hiragana: 'を' },
-		{ romanji: 'n', hiragana: 'ん' }
-	]
+  const [hiragana, setHiragana] = useState([]);
+  const [input, setInput] = useState('');
+  const [current, setCurrent] = useState(0);
+  const [streak, setStreak] = useState(0);
+  const [maxStreak, setMaxStreak] = useState(0);
+  const [error, setError] = useState(false);
 
-	const [input, setInput] = useState('')
-	const [current, setCurrent] = useState(0)
-	
-	const [streak, setStreak] = useState(0)
-	const [maxStreak, setMaxStreak] = useState(0)
+  useEffect(() => {
+    const fetchHiragana = async () => {
+      try {
+        // Fetch hiragana characters from the API
+        const response = await axios.get(
+          'https://japanese-alphabet.p.rapidapi.com/hiragana',
+          {
+            headers: {
+              'x-rapidapi-key': process.env.JAPANESE_API_KEY,
+              'x-rapidapi-host': 'japanese-alphabet.p.rapidapi.com',
+            },
+          }
+        );
+        console.log('API Response:', response.data);
+		setHiragana(response.data);
+        setCurrent(getRandomIndex(response.data.length));
+      } catch (error) {
+        console.error('Error fetching Hiragana characters:', error);
+      }
+    };
 
-	const [error, setError] = useState(false)
+    fetchHiragana();
+  }, []);
 
-	const setRandomHiragana = () => {
-		const randomIndex = Math.floor(Math.random() * hiragana.length)
-		setCurrent(randomIndex)
-	}
+  const getRandomIndex = (length) => {
+    return Math.floor(Math.random() * length);
+  };
 
-	const handleChange = (e) => {
-		setInput(e.target.value)
-	}
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		 
-		if (input.toLowerCase() === hiragana[current].romanji) {
-			setStreak(streak + 1)
-			setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak)
-			setError(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-			localStorage.setItem('streak', streak + 1)
-			localStorage.setItem('maxStreak', streak + 1 > maxStreak ? streak + 1 : maxStreak)
-		} else {
-			const h = hiragana[current].hiragana
-			const r = hiragana[current].romanji
-			setError(`Wrong! The correct answer for ${h} is ${r}`)
-			setStreak(0)
-			localStorage.setItem('streak', 0)
-		}
+    if (input.toLowerCase() === hiragana[current].romanji) {
+      setStreak(streak + 1);
+      setMaxStreak(streak + 1 > maxStreak ? streak + 1 : maxStreak);
+      setError(false);
 
-		setInput('')
-		setRandomHiragana()
-	}
+      localStorage.setItem('streak', streak + 1);
+      localStorage.setItem(
+        'maxStreak',
+        streak + 1 > maxStreak ? streak + 1 : maxStreak
+      );
+    } else {
+      const h = hiragana[current].hiragana;
+      const r = hiragana[current].romanji;
+      setError(`Wrong! The correct answer for ${h} is ${r}`);
+      setStreak(0);
+      localStorage.setItem('streak', 0);
+    }
 
-	useEffect(() => {
-		setRandomHiragana()
-		setStreak(parseInt(localStorage.getItem('streak')) || 0)
-		setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0)
-	}, [])
+    setInput('');
+    setCurrent(getRandomIndex(hiragana.length));
+  };
 
-	return (
-		<div className="min-h-screen bg-slate-800 text-white text-center">
-			<header className="p-6 mb-8">
-				<h1 className="text-2xl font-bold uppercase">Hiragana Quiz</h1>
-				<div>
-					<p>{streak} / {maxStreak}</p>
-				</div>
-			</header>
+  useEffect(() => {
+    setStreak(parseInt(localStorage.getItem('streak')) || 0);
+    setMaxStreak(parseInt(localStorage.getItem('maxStreak')) || 0);
+  }, []);
 
-			<div className="text-9xl font-bold mb-8">
-				<p>{hiragana[current].hiragana}</p>
-			</div>
+  return (
+    <div className="min-h-screen bg-slate-800 text-white text-center">
+      <header className="p-6 mb-8">
+        <h1 className="text-2xl font-bold uppercase">Hiragana</h1>
+        <div>
+          <p>
+            {streak} / {maxStreak}
+          </p>
+        </div>
+      </header>
 
-			<div className="mb-8">
-				<form onSubmit={handleSubmit}>
-					<input
-						type="text"
-						onChange={handleChange}
-						value={input}
-						className="block w-24 bg-transparent border-b-2 border-b-white mx-auto outline-none text-center text-6xl pb-2" />
-				</form>
-			</div>
-			{error && 
-				<div>
-					<p>{ error }</p>
-				</div>
-			}
-		</div>
-	)
+      <div className="text-9xl font-bold mb-8">
+        <p>{hiragana[current]?.hiragana}</p>
+      </div>
+
+      <div className="mb-8">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            value={input}
+            className="block w-24 bg-transparent border-b-2 border-b-white mx-auto outline-none text-center text-6xl pb-2"
+          />
+        </form>
+      </div>
+      {error && (
+        <div>
+          <p>{error}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
